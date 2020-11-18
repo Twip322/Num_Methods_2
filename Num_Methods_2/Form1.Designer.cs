36c252eb47dcbb3d5b94467cb09525cb13239028
Утицких Дмитрий <samprp458804@gmail.com>
2020-11-18 21:34:37 +0400

    slave

---
+++ Num_Methods_2/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Num_Methods_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBoxA = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxD = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxL = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxR = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxB = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxvecC = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxConver = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxiter = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(647, 79);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Якоби";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(647, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(644, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Эпсилон";
+            // 
+            // richTextBoxA
+            // 
+            this.richTextBoxA.Location = new System.Drawing.Point(0, 31);
+            this.richTextBoxA.Name = "richTextBoxA";
+            this.richTextBoxA.Size = new System.Drawing.Size(153, 139);
+            this.richTextBoxA.TabIndex = 3;
+            this.richTextBoxA.Text = "";
+            // 
+            // richTextBoxD
+            // 
+            this.richTextBoxD.Location = new System.Drawing.Point(159, 31);
+            this.richTextBoxD.Name = "richTextBoxD";
+            this.richTextBoxD.Size = new System.Drawing.Size(153, 139);
+            this.richTextBoxD.TabIndex = 4;
+            this.richTextBoxD.Text = "";
+            // 
+            // richTextBoxL
+            // 
+            this.richTextBoxL.Location = new System.Drawing.Point(318, 31);
+            this.richTextBoxL.Name = "richTextBoxL";
+            this.richTextBoxL.Size = new System.Drawing.Size(153, 139);
+            this.richTextBoxL.TabIndex = 5;
+            this.richTextBoxL.Text = "";
+            // 
+            // richTextBoxR
+            // 
+            this.richTextBoxR.Location = new System.Drawing.Point(477, 31);
+            this.richTextBoxR.Name = "richTextBoxR";
+            this.richTextBoxR.Size = new System.Drawing.Size(153, 139);
+            this.richTextBoxR.TabIndex = 6;
+            this.richTextBoxR.Text = "";
+            // 
+            // richTextBoxB
+            // 
+            this.richTextBoxB.Location = new System.Drawing.Point(0, 207);
+            this.richTextBoxB.Name = "richTextBoxB";
+            this.richTextBoxB.Size = new System.Drawing.Size(153, 139);
+            this.richTextBoxB.TabIndex = 7;
+            this.richTextBoxB.Text = "";
+            // 
+            // richTextBoxvecC
+            // 
+            this.richTextBoxvecC.Location = new System.Drawing.Point(159, 207);
+            this.richTextBoxvecC.Name = "richTextBoxvecC";
+            this.richTextBoxvecC.Size = new System.Drawing.Size(153, 139);
+            this.richTextBoxvecC.TabIndex = 8;
+            this.richTextBoxvecC.Text = "";
+            // 
+            // richTextBoxConver
+            // 
+            this.richTextBoxConver.Location = new System.Drawing.Point(318, 207);
+            this.richTextBoxConver.Name = "richTextBoxConver";
+            this.richTextBoxConver.Size = new System.Drawing.Size(153, 139);
+            this.richTextBoxConver.TabIndex = 9;
+            this.richTextBoxConver.Text = "";
+            // 
+            // richTextBoxiter
+            // 
+            this.richTextBoxiter.Location = new System.Drawing.Point(12, 389);
+            this.richTextBoxiter.Name = "richTextBoxiter";
+            this.richTextBoxiter.Size = new System.Drawing.Size(399, 169);
+            this.richTextBoxiter.TabIndex = 10;
+            this.richTextBoxiter.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 570);
+            this.Controls.Add(this.richTextBoxiter);
+            this.Controls.Add(this.richTextBoxConver);
+            this.Controls.Add(this.richTextBoxvecC);
+            this.Controls.Add(this.richTextBoxB);
+            this.Controls.Add(this.richTextBoxR);
+            this.Controls.Add(this.richTextBoxL);
+            this.Controls.Add(this.richTextBoxD);
+            this.Controls.Add(this.richTextBoxA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBoxA;
+        private System.Windows.Forms.RichTextBox richTextBoxD;
+        private System.Windows.Forms.RichTextBox richTextBoxL;
+        private System.Windows.Forms.RichTextBox richTextBoxR;
+        private System.Windows.Forms.RichTextBox richTextBoxB;
+        private System.Windows.Forms.RichTextBox richTextBoxvecC;
+        private System.Windows.Forms.RichTextBox richTextBoxConver;
+        private System.Windows.Forms.RichTextBox richTextBoxiter;
+    }
+}
+
---
+++ Num_Methods_2/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Num_Methods_2
+{
+    public partial class Form1 : Form
+    {
+        private float[,] systemlist =
+        {
+            {4.405F  ,0.472F   ,0.395F   ,0.0253F  ,0.623F },
+            {0.227F  ,2.957F   ,0.342F   ,0.327F   ,0.072F},
+            {0.419F  ,0.341F   ,3.238F   ,0.394F   ,0.143F },
+            {0.325F  ,0.326F   ,0.401F   ,4.273F  ,0.065F}
+        };
+        float[,] transsyslist;
+        float epsilon;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void transformed()
+        {
+            transsyslist = new float[systemlist.GetLength(0), systemlist.GetLength(1)];
+            int setter = 0;
+            for(int i=0;i< systemlist.GetLength(0);i++)
+            {
+                for (int j = 0; j < systemlist.GetLength(1); j++)
+                {
+                    transsyslist[i, j] = (float)Math.Round(systemlist[i, j] / systemlist[i, setter],4)*-1;
+                    if(transsyslist[i,j]==-1)
+                    {
+                        transsyslist[i, j] = 0;
+                    }
+                    if(j==systemlist.GetLength(1)-1)
+                    {
+                        transsyslist[i, j] = transsyslist[i, j]*-1;
+                    }
+                }
+                setter++;
+            }
+            
+            
+        }
+    private void matrixA()
+        {
+            string tmp="";
+            for (int i = 0; i < systemlist.GetLength(0); i++)
+            {
+                tmp = "";
+                for (int j = 0; j < systemlist.GetLength(1)-1; j++)
+                {
+                    tmp += systemlist[i, j] + "  ";
+                }
+                tmp += "\n\n";
+                richTextBoxA.Text += tmp;
+            }
+        }
+        private void matrixD()
+        {
+            string tmp = "";
+            int line=0;
+            for (int i = 0; i < systemlist.GetLength(0); i++)
+            {
+                tmp = "";
+                for (int j = 0; j < systemlist.GetLength(1)-1; j++)
+                {
+                    if(j==line)
+                    {
+                        tmp += systemlist[i, j] + "  ";
+                    }
+                    else
+                    {
+                        tmp += 0 + "  ";
+                    }
+                }
+                line++;
+                tmp += "\n\n";
+                richTextBoxD.Text += tmp;
+            }
+        }
+        private void matrixL()
+        {
+            string tmp = "";
+            int nums=0;
+            for (int i = 0; i < systemlist.GetLength(0); i++)
+            {
+                int tmpnums = nums + i;
+                tmp = "";
+                for (int j = 0; j < systemlist.GetLength(1) - 1; j++)
+                {
+                    if (tmpnums!=0)
+                    {
+                        tmp += systemlist[i, j] + "  ";
+                        tmpnums--;
+                    }
+                    else
+                    {
+                        tmp += 0 + "  ";
+                    }
+
+                }
+                tmp += "\n\n";
+                richTextBoxL.Text += tmp;
+            }
+        }
+        private void matrixR()
+        {
+            string tmp = "";
+            int nums = 1;
+            for (int i = 0; i < systemlist.GetLength(0); i++)
+            {
+                int tmpnums = nums + i;
+                tmp = "";
+                for (int j = 0; j < systemlist.GetLength(1) - 1; j++)
+                {
+                    if (tmpnums != 0)
+                    {
+                        tmp += 0 + "  ";
+                        tmpnums--;
+                    }
+                    else
+                    {
+                        tmp += systemlist[i, j] + "  ";
+
+                    }
+
+                }
+                tmp += "\n\n";
+                richTextBoxR.Text += tmp;
+            }
+        }
+        private void matrixB()
+        {
+            string tmp = "";
+            for (int i = 0; i < transsyslist.GetLength(0); i++)
+            {
+                tmp = "";
+                for (int j = 0; j < transsyslist.GetLength(1)-1; j++)
+                {
+                    tmp += transsyslist[i, j] + "  ";
+                }
+                tmp += "\n\n";
+                richTextBoxB.Text += tmp;
+            }
+        }
+        private void vectorC()
+        {
+            string tmp = "";
+            for (int i = 0; i < transsyslist.GetLength(0); i++)
+            {
+                tmp = "";
+                tmp += transsyslist[i, transsyslist.GetLength(1)-1] + "  ";
+                tmp += "\n\n";
+                richTextBoxvecC.Text += tmp;
+            }
+        }
+        private void convergences_check()
+        {
+            float x=0;
+            float y=0;
+            int setter=0;
+            for(int i=0; i<systemlist.GetLength(0);i++)
+            {
+                x = systemlist[i, setter];
+                for (int j = 0; j < systemlist.GetLength(1) - 1; j++)
+                {
+                    if(j!=setter)
+                    {
+                        y += systemlist[i, j];
+                    }
+                }
+
+                richTextBoxConver.Text += x+" > "+y;
+                if (x > y)
+                {
+                    richTextBoxConver.Text += "  true\n";
+                }
+                else
+                    richTextBoxConver.Text += "  false\n";
+                    setter++;
+                y = 0;
+            }
+            
+        }
+        private void iterations()
+        {
+            float epsilon = (float)Convert.ToDouble(textBox1.Text);
+            float max;
+            int k = 0;
+            float[] line = new float[transsyslist.GetLength(1) - 1];
+            for (int i = 0; i < line.Length; i++)
+            {
+                line[i] = 0;
+            }
+            float[] templine = new float[transsyslist.GetLength(1) - 1];
+            do
+            {
+                richTextBoxiter.Text += k + " ";
+                for (int i = 0; i < line.Length; i++)
+                {
+                    for (int j = 0; j < transsyslist.GetLength(1); j++)
+                    {
+                        if (transsyslist[i, j] != 0)
+                            templine[i] += transsyslist[i, j] * line[i];
+                        if (j == transsyslist.GetLength(1) - 1)
+                            templine[i] += transsyslist[i, j];
+                    }
+                    
+                    richTextBoxiter.Text += (float)Math.Round(templine[i], 4) + " ";
+                }
+                max = (float)Math.Round(Math.Max(Math.Max(Math.Abs(templine[0] - line[0]), Math.Abs(templine[1] - line[1])), Math.Max(Math.Abs(templine[2] - line[2]), Math.Abs(templine[0] - line[0]))),4);
+                richTextBoxiter.Text += max + "\n";
+                k++;
+                line = templine;
+                templine = new float[transsyslist.GetLength(1) - 1];
+            } while (max > epsilon);
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            transformed();
+            matrixA();
+            matrixD();
+            matrixL();
+            matrixR();
+            matrixB();
+            vectorC();
+            convergences_check();
+            iterations();
+        }
+
+       
+    }
+}
