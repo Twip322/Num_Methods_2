91cbc1bcd2c393cafbd9282cc84630edafdad84f
Утицких Дмитрий <samprp458804@gmail.com>
2020-12-24 09:46:43 +0400

    num7ready

--- Num_Methods_7/Form1.Designer.cs
+++ Num_Methods_7/Form1.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBoxSimpson = new System.Windows.Forms.RichTextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.richTextBoxTrapeze = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBoxSimpson
+            // 
+            this.richTextBoxSimpson.Location = new System.Drawing.Point(26, 177);
+            this.richTextBoxSimpson.Name = "richTextBoxSimpson";
+            this.richTextBoxSimpson.Size = new System.Drawing.Size(514, 148);
+            this.richTextBoxSimpson.TabIndex = 34;
+            this.richTextBoxSimpson.Text = "";
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(644, 137);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 31;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // richTextBoxTrapeze
+            // 
+            this.richTextBoxTrapeze.Location = new System.Drawing.Point(26, 12);
+            this.richTextBoxTrapeze.Name = "richTextBoxTrapeze";
+            this.richTextBoxTrapeze.Size = new System.Drawing.Size(514, 148);
+            this.richTextBoxTrapeze.TabIndex = 30;
+            this.richTextBoxTrapeze.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxSimpson);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.richTextBoxTrapeze);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxSimpson;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.RichTextBox richTextBoxTrapeze;
     }
 }
 
--- Num_Methods_7/Form1.cs
+++ Num_Methods_7/Form1.cs
@@ -12,9 +12,119 @@ namespace Num_Methods_7
 {
     public partial class Form1 : Form
     {
+        public int a = 0;
+        public int b = 2;
+        public double h = 0.166;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Trapeze()
+        {
+            double offset = a;
+            double answer = 0;
+
+            while (offset < b)
+            {
+                if (offset == a)
+                {
+                    answer += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2)))) / 2;
+                }
+                else if (offset + h > b)
+                {
+                    answer += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2)))) / 2;
+                }
+                else answer += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2))));
+                offset += h;
+            }
+            answer *= h;
+            richTextBoxTrapeze.Text += $"При h={h} формула трапеций будет иметь значение: {Math.Round(answer, 4)}\n";
+            offset = a;
+            double hDouble = h * 2;
+            double answerDouble = 0;
+            while (offset < b)
+            {
+                if (offset == a)
+                {
+                    answerDouble += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2)))) / 2;
+                }
+                else if (offset + h > b)
+                {
+                    answerDouble += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2)))) / 2;
+                }
+                else answerDouble += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2))));
+                offset += hDouble;
+            }
+            answerDouble *= hDouble;
+            richTextBoxTrapeze.Text += $"При h={hDouble} формула трапеций будет иметь значение: {Math.Round(answerDouble, 4)}\n";
+            double rule = Math.Abs(answer - answerDouble) / 3;
+            richTextBoxTrapeze.Text += $"Вычислив по правилу Рунге погрешность, получаем : {Math.Round(rule, 5)}\n";
+        }
+        private void Simpson()
+        {
+            double offset = a;
+            double answer = 0;
+            double testoffset = a;
+            while (testoffset < b-h)
+            {
+                testoffset += h;
+            }
+            while (offset < b)
+            {
+                if (offset == a)
+                {
+                    answer += ((Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2)))) + (Math.Pow(testoffset, 3) / (1 + (Math.Pow(testoffset, 2))))) / 2;
+                }
+                else if (offset - h == a)
+                {
+                    answer += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2)))) * 2;
+                }
+                else if (offset + h > b)
+                {
+                    answer += (Math.Pow(offset-h, 3) / (1 + (Math.Pow(offset - h, 2)))) * 2;
+                }
+                else answer += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2))));
+                offset += h;
+            }
+            answer *= (2 * h) / 3;
+            richTextBoxSimpson.Text += $"При h={h} формула Симпсона будет иметь значение: {Math.Round(answer, 4)}\n";
+            offset = a;
+            double hDouble = h * 2;
+            double answerDouble = 0;
+            testoffset = a;
+            while (testoffset < b - hDouble)
+            {
+                testoffset += hDouble;
+            }
+            while (offset < b)
+            {
+                if (offset == a)
+                {
+                    answerDouble += ((Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2)))) + (Math.Pow(testoffset, 3) / (1 + (Math.Pow(testoffset, 2))))) / 2;
+                }
+                else if (offset - hDouble == a)
+                {
+                    answerDouble += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2)))) * 2;
+                }
+                else if (offset + h > b)
+                {
+                    answerDouble += (Math.Pow(offset - hDouble, 3) / (1 + (Math.Pow(offset - hDouble, 2)))) * 2;
+                }
+                else answerDouble += (Math.Pow(offset, 3) / (1 + (Math.Pow(offset, 2))));
+                offset += hDouble;
+            }
+            answerDouble *= (2 * hDouble) / 3;
+            richTextBoxSimpson.Text += $"При h={hDouble} формула Симпсона будет иметь значение: {Math.Round(answerDouble, 4)}\n";
+            double rule = Math.Abs(answer - answerDouble) / 15;
+            richTextBoxSimpson.Text += $"Вычислив по правилу Рунге погрешность, получаем : {Math.Round(rule, 7)}\n";
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            richTextBoxSimpson.Text = "";
+            richTextBoxTrapeze.Text = "";
+            Trapeze();
+            Simpson();
         }
     }
 }
