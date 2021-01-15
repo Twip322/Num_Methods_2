1365443bb1e811797a0ec960d8c48ec07a723982
Утицких Дмитрий <samprp458804@gmail.com>
2021-01-15 11:39:28 +0400

    start6

--- Num_Mehods_6/Form1.cs
+++ Num_Mehods_6/Form1.cs
@@ -21,8 +21,56 @@ namespace Num_Mehods_6
         double[] xLine = { 0.351,0.664,0.978,1.291,1.605,1.918,2.232,2.546,2.859 };
         double[] yLine = { 0.605, 0.265, 0.064, 0.116, 0.415, 0.728, 1.673, 3.138, 5.092 };
         double x0;
+
+
+        private double triangle(double[] triangleFunX)
+        {
+            double triunder = 0;
+            return triunder = triangleFunX[0] * triangleFunX[4] * triangleFunX[8] + triangleFunX[1] * triangleFunX[5] * triangleFunX[6] +
+                triangleFunX[2] * triangleFunX[3] * triangleFunX[7] - triangleFunX[2] * triangleFunX[4] * triangleFunX[6] - triangleFunX[0] * triangleFunX[5] * triangleFunX[7] - triangleFunX[1] * triangleFunX[4] * triangleFunX[8];
+        }
         private void approximationFirst()
         {
+            double x4=0;
+            for(int i=0;i<xLine.Length; i++)
+            {
+                x4 += Math.Pow(xLine[i], 4);
+            }
+            double x3 = 0;
+            for (int i = 0; i < xLine.Length; i++)
+            {
+                x3 += Math.Pow(xLine[i], 3);
+            }
+            double x2 = 0;
+            for (int i = 0; i < xLine.Length; i++)
+            {
+                x2 += Math.Pow(xLine[i], 2);
+            }
+            double x2y = 0;
+            for (int i = 0; i < xLine.Length; i++)
+            {
+                x2y += Math.Pow(xLine[i], 2)*yLine[i];
+            }
+            double xy = 0;
+            for (int i = 0; i < xLine.Length; i++)
+            {
+                xy += xLine[i]*yLine[i];
+            }
+            double x = 0;
+            for (int i = 0; i < xLine.Length; i++)
+            {
+                x += xLine[i];
+            }
+            double y = 0;
+            for (int i = 0; i < xLine.Length; i++)
+            {
+                y += yLine[i];
+            }
+            double[] triangleFunX = {x4,x3,x2,x3,x2,x,x2,x,9};
+            double[] triangleFunY = {x2y,xy,y};
+            double triangleUnder = triangle(triangleFunX);
+            double[] tmpTrianleA=triangleFunX;
+
             double a = 1.6162;
             double b = -3.5722;
             double c = 1.8549;
